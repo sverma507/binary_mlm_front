7bd026f1b8a6381fadf820e7a79b2b64f3481a33
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2024-09-18 10:56:23 +0530

    added

---
+++ binary-mlm/src/pages/signup/signup.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./signup.css"; // Import the CSS file
+
+const LoginSignup = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
+  return (
+    <div className="signup-login-container">
+    <div className={`container ${isActive ? "active" : ""}`} id="container">
+      {/* Sign-Up Form */}
+      <div className="form-container sign-up">
+        <form>
+          <h1>Create Account</h1>
+          {/* <div className="social-icons">
+            <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+          </div> */}
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+
+      {/* Sign-In Form */}
+      <div className="form-container sign-in">
+        <form>
+          <h1 className="heading-login">Sign In</h1>
+          {/* <div className="social-icons">
+            <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+          </div>
+          <span>or use your email for sign-in</span> */}
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forget Your Password?</a>
+          <button>Sign In</button>
+        </form>
+      </div>
+
+      {/* Toggle Container */}
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>Enter your personal details to use all of the site features</p>
+            <button className="hidden" onClick={handleLoginClick} id="login">
+              Sign In
+            </button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>Register with your personal details to use all of the site features</p>
+            <button className="hidden" onClick={handleRegisterClick} id="register">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
